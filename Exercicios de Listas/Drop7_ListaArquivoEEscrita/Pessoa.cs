a2650c94e96e6777b03d1ddba6e7a259e77cab21
RodrigoRPaulino <rodrigoramospaulino@gmail.com>
2022-07-12 00:41:02 -0300

    Adicionando exercicios com listas

---
+++ Exercicios de Listas/Drop7_ListaArquivoEEscrita/Pessoa.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drop7_ListaArquivoEEscrita
+{
+    internal class Pessoa
+    {
+       public  string nome;
+        public string email;
+
+        //método construtor
+      public Pessoa(string n, string e)
+        {
+            nome = n;
+            email = e;
+        }
+    }
+}
