e873feec4bb195b3ec03175ed30844bad0df4c2e
RodrigoRPaulino <rodrigoramospaulino@gmail.com>
2022-07-14 12:10:27 -0300

    finalizando botoes e algumas validações

--- EntityFrameWork/ProjetoFinal_RodrigoPaulino/Controllers/ListagemLojas.cs
+++ EntityFrameWork/ProjetoFinal_RodrigoPaulino/Controllers/ListagemLojas.cs
@@ -23,7 +23,7 @@ namespace ProjetoFinal_RodrigoPaulino.Controllers
             List<LojasModel> lojas = _repositorioLojas.ExibirTodos();
             return View(lojas);
         }
-      
+
         public IActionResult Cadastrar()
         {
             return View();
@@ -31,7 +31,7 @@ namespace ProjetoFinal_RodrigoPaulino.Controllers
         // aqui eu chamo o método de buscar os elementos  pelo id
         public IActionResult Editar(int id)
         {
-          LojasModel lojas = _repositorioLojas.BuscarId(id);
+            LojasModel lojas = _repositorioLojas.BuscarId(id);
             return View(lojas);
         }
         public IActionResult ApagarConfirmar(int id)
@@ -43,30 +43,82 @@ namespace ProjetoFinal_RodrigoPaulino.Controllers
         /// método que apaga de fato o registro do banco
         /// </summary>
         /// <returns></returns>
-        public IActionResult Apagar(int id) 
+        public IActionResult Apagar(int id)
         {
-            _repositorioLojas.Apagar(id);
-            return RedirectToAction("Index");
+            try
+            {
+                bool apagado =_repositorioLojas.Apagar(id);
+                if (apagado)
+                {
+                    TempData["MenssagemSucesso"] = "Informações apagadas com sucesso";
+                }
+                else
+                {
+                    TempData["MenssagemErro"] = $"Erro ao apagar as informações, tente novamente mais tarde";
+                }
+                return RedirectToAction("Index");
+            }
+            catch (Exception erro)
+            {
+
+                TempData["MenssagemErro"] = $"Erro ao apagar as informações, tente novamente mais tarde,{erro.Message}";
+                //retorno redirecionando a index 
+                return RedirectToAction("Index");
+            }
         }
 
         //vou informar que esse método e do tipo HttpPost
         [HttpPost]
         //método para criar lojas
         public IActionResult Cadastrar(LojasModel lojas)
-        {
+        {    
+            try
+            {
+                //criando condição : se o valor da modelstate for valida o if ira passar os comandos de cadastro
+                if (ModelState.IsValid)
+                {
+                    _repositorioLojas.Cadastrar(lojas);
+                    // criando menssagem temporária para exibir o exito no cadastro
+                    TempData["MenssagemSucesso"] = "Cadastro realizado com sucesso";
+                    //retorno redirecionando a index 
+                    return RedirectToAction("Index");
+                }
+                return View(lojas);
+            }
+            catch (System.Exception erro)
+            {
 
-            _repositorioLojas.Cadastrar(lojas);
-            //retorno redirecionando a index 
-            return RedirectToAction("Index");
+                TempData["MenssagemErro"] = $"Erro ao efetuar o cadastro, tente novamente mais tarde, {erro.Message}";
+                //retorno redirecionando a index 
+                return RedirectToAction("Index");
+            }
+                
+
         }
         [HttpPost]
         //método para adicionar lojas
         public IActionResult Alterar(LojasModel lojas)
         {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _repositorioLojas.Atualizar(lojas);
+                    TempData["MenssagemSucesso"] = "Alterações realizadas com sucesso";
+                    //retorno redirecionando a index 
+                    return RedirectToAction("Index");
+                }
+                //forçamos o retorno da view 
+                return View("Editar", lojas);
+            }
+            catch (System.Exception erro)
+            {
 
-            _repositorioLojas.Atualizar(lojas);
-            //retorno redirecionando a index 
-            return RedirectToAction("Index");
+                TempData["MenssagemErro"] = $"Erro ao alterar as informações, tente novamente mais tarde, {erro.Message}";
+                //retorno redirecionando a index 
+                return RedirectToAction("Index");
+            }
+            
         }
 
     }
--- EntityFrameWork/ProjetoFinal_RodrigoPaulino/Models/LojasModel.cs
+++ EntityFrameWork/ProjetoFinal_RodrigoPaulino/Models/LojasModel.cs
@@ -1,14 +1,22 @@
 ﻿
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ProjetoFinal_RodrigoPaulino.Models
 {
+    //forçando o programa a criar a tabela lojas
     [Table("lojas")]
     public class LojasModel
     {
         public int Id { get; set; }
-        public string? NomeLoja { get; set; }
-        public string? Localizacao { get; set; }
-        public string? Telefone { get; set; }
+        //validando o campo como obrigatório exibindo uma menssagem de erro 
+        [Required(ErrorMessage ="Digite um nome válido")]
+        public string NomeLoja { get; set; }
+        [Required(ErrorMessage ="Digite um endereço válido")]
+        public string Localizacao { get; set; }
+        //validando o campo telefone como obrigatório e fazendo tambem um phone error message
+        [Required(ErrorMessage ="Digite um telefone válido")]
+        [Phone(ErrorMessage ="Digite um numero de telefone válido")]
+        public string Telefone { get; set; }
     }
 }
